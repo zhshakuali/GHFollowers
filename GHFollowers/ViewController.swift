4737eb99d4a173c4b5957c37b09ff7cf008e83c6
zhshakuali <zhansayashakuali200309@gmail.com>
2023-07-02 14:48:55 +0600

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Жансая Шакуали on 02.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
